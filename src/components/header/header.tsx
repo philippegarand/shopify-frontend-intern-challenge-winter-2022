9473a701de2d09337c80e7e0a9c6fedb0a91a757
Philippe Garand <philippe.garand@hotmail.com>
2021-09-10 22:26:11 -0400

    changed app logo and styled appbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,34 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import {
+  AppBar,
+  Container,
+  makeStyles,
+  SvgIcon,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import { ReactComponent as LogoSvg } from '../../assets/logo.svg'
+
+const useStyles = makeStyles({
+  spaceBanner: {
+    backgroundImage:
+      'url("https://www.nasa.gov/sites/default/files/thumbnails/image/stsci-h-p1917b-q-5198x4801.jpg")',
+    backgroundColor: 'black',
+  },
+  icon: {
+    marginRight: '8px',
+  },
+})
 
 export default function Header() {
+  const classes = useStyles()
+
   return (
-    <AppBar position="sticky">
+    <AppBar position="sticky" classes={{ root: classes.spaceBanner }}>
       <Container maxWidth="md">
-        <Toolbar>
+        <Toolbar disableGutters>
+          <SvgIcon classes={{ root: classes.icon }}>
+            <LogoSvg />
+          </SvgIcon>
           <Typography component="h1" variant="h6">
             Spacestagram
           </Typography>
